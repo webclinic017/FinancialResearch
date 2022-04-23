f0581c4da3895b4d5eb784531fdec93d7f080f04
ppoak <oakery@qq.com>
2022-04-23 14:26:36 +0800

    [update] 添加analyst中regressor角色与describer角色

--- pandasquant/staff/analyst.py
+++ pandasquant/staff/analyst.py
@@ -4,7 +4,52 @@ import statsmodels.api as sm
 from ..tools import *
 
 
-class Analyzer(object):
+@pd.api.extensions.register_dataframe_accessor("regressor")
+class Regressor(Worker):
+
+    def ols(self, x_col: 'list', y_col: 'str'):
+        ''''''
+        def _reg(d):
+            x = d.loc[:, x_col]
+            x = sm.add_constant(x)
+            y = d.loc[:, y_col]
+            model = sm.OLS(y, x).fit()
+            t = pd.Series(model.tvalues)
+            coef = pd.Series(model.params)
+            return pd.concat([t, coef], axis=1)
+        
+        if self.type_ == Worker.TIMESERIES:
+            return _reg(self.dataframe)
+        
+        elif self.type_ == Worker.PANEL:
+            return self.dataframe.groupby(level=0).apply(_reg)
+            
+        else:
+            raise TypeError('ols currently only support for panel and timeseries data')
+
+@pd.api.extensions.register_dataframe_accessor("describer")
+class Describer(Worker):
+
+    def corr(self, method: str = 'spearman', tvalue = False):
+        if self.type_ != Worker.PANEL:
+            return self.dataframe.corr(method=method)
+        
+        else:
+            tcor = self.dataframe.groupby(level=0).corr()
+            if tvalue:
+                n = tcor.index.levels[0].size
+                mean = tcor.groupby(level=0).mean()
+                std = tcor.groupby(level=0).std()
+                t = mean / std * np.sqrt(n)
+                return t
+            else:
+                return tcor
+
+@pd.api.extensions.register_dataframe_accessor("position")
+class Position(Worker):
+    pass
+
+class _Analyzer(object):
     '''Analyzer is a general analyst dedicated to analyze the AnalyzeData
     =====================================================================
 
@@ -261,6 +306,6 @@ if __name__ == "__main__":
     prices = PanelFrame(assets=prices)
     groups = PanelFrame(datetimes=groups)
 
-    analyzer = Analyzer(factor=factors, price=prices, group=groups, infer_forward='5d')
+    analyzer = _Analyzer(factor=factors, price=prices, group=groups, infer_forward='5d')
     analyzer.regression()
     analyzer.regression_plot(show=True)
