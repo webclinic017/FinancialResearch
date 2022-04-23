bbaa5ff7ba7cd5b96cb2898bd6dfe168b520c6ab
ppoak <oakery@qq.com>
2022-04-23 21:55:39 +0800

    删除convert脚本，功能融合入pandasquant.staff.preprocessor中

--- pandasquant/tools/__init__.py
+++ pandasquant/tools/__init__.py
@@ -1,23 +1,10 @@
 from .core import (
-    PanelFrame, Worker
-    )
-
-from .converter import (
-    datetime2str, str2datetime, 
-    logret2algret, algret2logret, 
-    item2list, periodkey,
-    price2ret, price2fwd,
-    category2dummy, dummy2category,
-    cum2diff
+    PanelFrame, 
+    Worker
     )
 
 
 __all__ = [
-    'PanelFrame', 'Worker',
-    'datetime2str', 'str2datetime',
-    'logret2algret', 'algret2logret',
-    'item2list', 'periodkey',
-    'price2ret', 'price2fwd',
-    'category2dummy', 'dummy2category',
-    'cum2diff'
+    'PanelFrame', 
+    'Worker'
     ]
