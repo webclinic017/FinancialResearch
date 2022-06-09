f0de00349752d433726ff2f590c5f5f7ee977b58
oak <oakery@qq.com>
2022-06-10 07:10:48 +0800

    [UPD] 更新pq版本，重构viewer架构，以bt为基础

---
+++ viewer/datas.py
@@ -0,0 +1,30 @@
+import akshare as ak
+import pandas as pd
+import pandasquant as pq
+import backtrader as bt
+
+
+@pq.RedisCache(prefix='viewer')
+def stockfeedapi(code: str, start: str, end: str, fromdate: str = None, todate: str = None):
+    data = pq.Api.market_daily(start, end, fields=['t_tradingDate', 'c_windCode', 
+        'n_adjOpen', 'n_adjHigh', 'n_adjLow', 'n_adjClose', 'n_volume'],
+        conditions='c_windCode=%s' % code).droplevel(1)
+    fromdate = pq.str2time(fromdate) if fromdate else data.index[0]
+    todate = pq.str2time(todate) if todate else data.index[-1]
+    feed = bt.feeds.PandasData(dataname=data, fromdate=fromdate, todate=todate)
+    return feed
+
+@pq.RedisCache(prefix='viewer')
+def etffeedsina(code: str, fromdate: str = None, todate: str = None):
+    data = ak.fund_etf_hist_sina(symbol=code)
+    data = data.rename(columns={'date': 'datetime'})
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    data = data.set_index('datetime')
+    fromdate = pq.str2time(fromdate) if fromdate else data.index[0]
+    todate = pq.str2time(todate) if todate else data.index[-1]
+    feed = bt.feeds.PandasData(dataname=data, fromdate=fromdate, todate=todate)
+    return feed
+
+
+if __name__ == '__main__':
+    print(etffeedsina('sh510300'))
--- viewer/view.py
+++ viewer/view.py
@@ -1,21 +1,59 @@
+import pandas as pd
 import backtrader as bt
 import pandasquant as pq
+from .datas import *
+from .indicators import *
+from .strategies import *
 
 
-if __name__ == '__main__':
-    # Get dataset
-    data = pq.Feed.from_api('600392.SH', '20170101', '20220601')
-    # Create a cerebro entity
-    cerebro = bt.Cerebro(stdstats=False)
-    # Feed data to the cerebro
-    cerebro.adddata(data)
-    # Add indicators
-    cerebro.addindicator(bt.indicators.SMA, period=5)
-    cerebro.addindicator(bt.indicators.SMA, period=10)
-    cerebro.addindicator(bt.indicators.SMA, period=20)
-    cerebro.addindicator(bt.indicators.SMA, period=250)
-    cerebro.addindicator(bt.indicators.ATR, period=10)
-    # Visualization
-    cerebro.run()
-    cerebro.plot(style='candle')
-    
+# Get dataset
+data = etffeedsina('sh510300')
+
+# Create a cerebro entity
+cerebro = bt.Cerebro()
+
+# Cheat on close
+cerebro.broker.set_coc(True)
+
+# Add money
+cerebro.broker.setcash(1000000)
+
+# Feed data to the cerebro
+cerebro.adddata(data)
+
+# Add indicators
+# cerebro.addindicator(Grid, period=20)
+
+# Add strategy
+cerebro.addstrategy(GridStrategy)
+# cerebro.addstrategy(SMACrossStrategy)
+
+# Add observers
+cerebro.addobserver(bt.observers.DrawDown)
+cerebro.addobserver(bt.observers.Benchmark)
+
+# Add analyzers
+cerebro.addanalyzer(bt.analyzers.SharpeRatio)
+cerebro.addanalyzer(bt.analyzers.TimeDrawDown)
+cerebro.addanalyzer(bt.analyzers.TimeReturn)
+cerebro.addanalyzer(pq.OrderTable)
+
+# Run the strategy
+result = cerebro.run()
+
+# Show analyze results
+timereturn = pd.Series(result[0].analyzers.timereturn.rets)
+print(result[0].analyzers.sharperatio.rets)
+print(result[0].analyzers.timedrawdown.rets)
+print(timereturn)
+print(result[0].analyzers.ordertable.rets)
+
+# Visualize the total strategy result
+cerebro.plot(style='candle')
+
+# Visualize the networth curve
+with pq.Gallery(1, 1, figsize=(12, 8)) as g:
+    ax = g.axes[0, 0]
+    (timereturn + 1).cumprod().drawer.draw(kind='line', ax=ax)
+    ax.hlines(xmin=ax.get_xlim()[0], xmax=ax.get_xlim()[1], y=1, color='grey', linestyle='--')
+    timereturn.drawer.draw(kind='bar', ax=ax.twinx())
